29c02dc3e956c3e3d04a19a9fddd974c7e6840e1
mirea70 <deae670@daum.net>
2025-05-21 16:26:48 +0900

    feat: Kafka Setting And HotArticle Repository

---
+++ service/hot-article/src/main/java/kuke/board/hotarticle/repository/HotArticleListRepository.java
@@ -0,0 +1,56 @@
+package kuke.board.hotarticle.repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class HotArticleListRepository {
+    private final StringRedisTemplate redisTemplate;
+
+    // hot-article::list::{yyyyMMdd}
+    private static final String KEY_FORMAT = "hot-article::list::%s";
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    public void add(Long articleId, LocalDateTime time, Long score, Long limit, Duration ttl) {
+        // Redis에 통신을 한번만 하면서 여러개의 연산 수행
+        redisTemplate.executePipelined((RedisCallback<?>) action -> {
+            StringRedisConnection conn = (StringRedisConnection) action;
+            String key = generateKey(time);
+            conn.zAdd(key, score, String.valueOf(articleId));
+            conn.zRemRange(key, 0, - limit - 1); // 상위 limit 개수만큼 sortedSet에 데이터 유지
+            conn.expire(key, ttl.toSeconds());
+            return null;
+        });
+    }
+
+    public List<Long> readAll(String dateStr) {
+        return redisTemplate.opsForZSet()
+                .reverseRangeWithScores(generateKey(dateStr), 0, -1).stream()
+                .peek(
+                        tuple -> log.info("[HotArticleListRepository.readAll] articleId={}, score={}", tuple.getValue(), tuple.getScore())
+                )
+                .map(ZSetOperations.TypedTuple::getValue)
+                .map(Long::valueOf)
+                .toList();
+    }
+
+    private String generateKey(LocalDateTime time) {
+        return generateKey(TIME_FORMATTER.format(time));
+    }
+
+    private String generateKey(String dateStr) {
+        return KEY_FORMAT.formatted(dateStr);
+    }
+}
