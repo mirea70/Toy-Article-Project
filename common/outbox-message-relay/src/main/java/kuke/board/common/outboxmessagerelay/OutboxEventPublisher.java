c7424899cf00a19e378193345e0c0890fd2cb95e
mirea70 <deae670@daum.net>
2025-05-24 23:23:32 +0900

    feat: Transactional OutBox Module

---
+++ common/outbox-message-relay/src/main/java/kuke/board/common/outboxmessagerelay/OutboxEventPublisher.java
@@ -0,0 +1,31 @@
+package kuke.board.common.outboxmessagerelay;
+
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventPayload;
+import kuke.board.common.event.EventType;
+import kuke.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OutboxEventPublisher {
+    private final Snowflake outboxIdSnowFlake = new Snowflake();
+    private final Snowflake eventIdSnowFlake = new Snowflake();
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public void publish(EventType type, EventPayload payload, Long shardKey) {
+        Outbox outbox = Outbox.create(
+                outboxIdSnowFlake.nextId(),
+                type,
+                Event.of(
+                        eventIdSnowFlake.nextId(),
+                        type,
+                        payload
+                ).toJson(),
+                shardKey % MessageRelayConstants.SHARD_COUNT
+        );
+        applicationEventPublisher.publishEvent(OutboxEvent.of(outbox));
+    }
+}
