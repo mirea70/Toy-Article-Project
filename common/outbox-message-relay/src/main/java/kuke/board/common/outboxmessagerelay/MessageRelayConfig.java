5a2c957bf4ad8c9ddec749ceb0083a3b34e57483
GrowMyself <deae670@daum.net>
2025-07-08 19:57:22 +0900

    fix: Outbox Message Relay Scheduler Type modify

--- common/outbox-message-relay/src/main/java/kuke/board/common/outboxmessagerelay/MessageRelayConfig.java
+++ common/outbox-message-relay/src/main/java/kuke/board/common/outboxmessagerelay/MessageRelayConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -16,6 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 @EnableAsync
 @Configuration
@@ -46,7 +48,7 @@ public class MessageRelayConfig {
     }
 
     @Bean
-    public Executor messageRelayPublishPendingEventExecutor() {
+    public ScheduledExecutorService messageRelayPublishPendingEventExecutor() {
         return Executors.newSingleThreadScheduledExecutor();
     }
 }
